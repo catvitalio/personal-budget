3db1e2a2ef8f6d40a724a28313030c5603d9ed1a
catvitalio <catvitalio@gmail.com>
2021-02-23 14:57:28 +0700

    topbar implemented

--- client/app/src/App.vue
+++ client/app/src/App.vue
@@ -1,32 +1,17 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link :to="{name: 'home'}">Бюджет</router-link> |
-      <router-link :to="{name: 'login'}">Войти</router-link>
-    </div>
+  <div>
+    <app-topbar />
     <router-view />
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import AppTopbar from '@/components/Topbar'
 
-#nav {
-  padding: 30px;
+export default {
+  name: 'App',
+  components: {
+    AppTopbar
+  }
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #007bff;
-}
-</style>
+</script>
---
+++ client/app/src/components/Topbar.vue
@@ -0,0 +1,53 @@
+<template>
+  <nav class="navbar navbar-expand-sm navbar-light">
+    <router-link class="navbar-brand mb-0 h1" :to="{name: 'home'}">
+      Personal Budget
+    </router-link>
+
+    <ul class="nav navbar-nav ml-auto">
+      <template v-if="!isLoggedIn">
+        <li class="nav-item">
+          <router-link
+            class="nav-link"
+            :to="{name: 'login'}"
+            active-class="active"
+          >
+            Войти
+          </router-link>
+        </li>
+      </template>
+
+      <template v-if="isLoggedIn">
+        <li class="nav-item">
+          <router-link class="nav-link" :to="{name: 'home'}">
+            Расходы
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" :to="{name: 'home'}">
+            Доходы
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" :to="{name: 'home'}">
+            {{ currentUser }}
+          </router-link>
+        </li>
+      </template>
+    </ul>
+  </nav>
+</template>
+
+<script>
+import {mapState} from 'vuex'
+
+export default {
+  name: 'AppTopbar',
+  computed: {
+    ...mapState({
+      currentUser: state => state.auth.currentUser,
+      isLoggedIn: state => state.auth.isLoggedIn
+    })
+  }
+}
+</script>
