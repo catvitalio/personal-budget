d64778e8c802ef48d6480c2f15bb3a297656c034
catvitalio <catvitalio@gmail.com>
2021-02-23 16:36:23 +0700

    GET method for users route added

---
+++ server/app/users/permissions/users.py
@@ -0,0 +1,8 @@
+from rest_framework import permissions
+
+
+class UserPermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method == 'GET':
+            return request.user.is_authenticated
+        return True
--- server/app/users/viewsets/users.py
+++ server/app/users/viewsets/users.py
@@ -1,13 +1,19 @@
-from rest_framework.mixins import CreateModelMixin
+from rest_framework.mixins import CreateModelMixin, ListModelMixin
 from rest_framework.viewsets import GenericViewSet, ViewSet
 from rest_framework.authtoken.serializers import AuthTokenSerializer
+from django.contrib.auth.models import User
 
 from users.serializers import UserSerializer
 from users.viewsets.custom import CustomObtainAuthToken
+from users.permissions import UserPermission
 
 
-class UserViewSet(CreateModelMixin, GenericViewSet):
+class UserViewSet(ListModelMixin, CreateModelMixin, GenericViewSet):
+    permission_classes = [UserPermission]
     serializer_class = UserSerializer
+
+    def get_queryset(self):
+        return User.objects.filter(id=self.request.user.id)
 
 
 class AuthViewSet(ViewSet):
