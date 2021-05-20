66e2d1b0cfa774f54e2b8968633c6ef850396c64
catvitalio <catvitalio@gmail.com>
2021-05-21 06:46:31 +0700

    ExitButton component implemented

---
+++ client/app/src/components/ExitButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="page">
+    <router-link :to="{name: 'expensesList'}"
+      ><button class="exit-button">✕</button></router-link
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppExitButton'
+}
+</script>
+
+<style lang="scss">
+.exit-button {
+  font-size: $--font-size-subtitle;
+  border: none;
+  border-radius: 50%;
+  width: 35px;
+  height: 35px;
+  background-color: $--primary;
+  transition: $--transition;
+  cursor: pointer;
+  &:hover {
+    background-color: $--secondary;
+    transition: $--transition;
+  }
+}
+</style>
